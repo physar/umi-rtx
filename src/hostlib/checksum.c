3b2af97fc83a3f23496dcf1ece028237546c1b21
Arnoud Visser <physar@gmail.com>
2022-03-01 14:32:10 +0100

    The code needed to created the rtx-daemon rtxd.
    To be tested with the RTX-UMI

---
+++ src/hostlib/checksum.c
@@ -0,0 +1,18 @@
+/*
+ * $Id$
+ *
+ * compute a checksum over nbytes starting at addr.
+ */
+int
+checksum(addr,len)
+char *addr;
+unsigned long len;
+{
+    unsigned long i;
+    unsigned int sum = 0;
+
+    for (i = 0; i < len; i++,addr++)
+	sum += *addr & 0xff;
+
+    return sum % 0xffff;
+}
